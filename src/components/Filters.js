bc470a56779d9c691d5f276c602be85cc5fc76f2
otinomz <awesomeotinomz@gmail.com>
2022-01-07 13:47:32 +0100

    milestone baby

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,65 @@
 import React from 'react'
+import { Button, Form} from 'react-bootstrap';
 
 const Filters = () => {
     return (
-        <div>
-            
-        </div>
-    )
+    <div className="filters">
+      <span className="title">Filter Products</span>
+      <span>
+        <Form.Check
+          inline
+          label="Ascending"
+          name="group1"
+          type="radio"
+          id={`inline-1`}
+          
+        //   checked={sort === "lowToHigh" ? true : false}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Descending"
+          name="group1"
+          type="radio"
+          id={`inline-2`}
+            //  checked={sort === "highToLow" ? true : false}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Include Out of Stock"
+          name="group1"
+          type="checkbox"
+          id={`inline-3`}
+        // checked={byStock}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Fast Delivery Only"
+          name="group1"
+          type="checkbox"
+          id={`inline-4`}
+        //   checked={byFastDelivery}
+        />
+      </span>
+      <span>
+        <label style={{ paddingRight: 10 }}>Rating: </label>
+        {/* <Rating
+        //   rating={byRating}
+          style={{ cursor: "pointer" }}
+        /> */}
+      </span>
+      <Button
+        variant="light"
+      >
+        Clear Filters
+      </Button>
+    </div>
+  );
 }
 
 export default Filters
