99bc13252828337668964e7e27dc4b11b189c583
otinomz <awesomeotinomz@gmail.com>
2022-01-07 15:05:44 +0100

    new fixes

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -5,13 +5,16 @@ const Rating = ({rating, onClick, style}) => {
     return (
         <>
             {/* since we dont want anythinng from the array,
-            we are going to drop an underscore and take the index */}
+            we are going to drop an underscore and take the index
+            simply writing an algorith. when you map through the empty array,
+            if the rating passes as props is greater than the current index, drop a fillStar icon
+            else if it isnt greater than the index, drop an airOutlineStar icon */}
             {[...Array(5)].map((_, index) => (
                 <span>
                     {rating > index ? (
                        <AiFillStar fontSize="15px"/> 
                     ) : (
-                        <AiOutlineStar fontSize="15px"/>
+                        <AiOutlineStar fo ntSize="15px"/>
                     )}
                 </span>
            ))} 
