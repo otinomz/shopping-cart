18246ff1b0362bf32fda36b8176056cff7e924ff
otinomz <awesomeotinomz@gmail.com>
2022-01-07 11:44:02 +0100

    new fix

--- src/context/Context.js
+++ src/context/Context.js
@@ -11,7 +11,7 @@ const Context = ({ children }) => {
         id: faker.datatype.uuid(), 
         name: faker.commerce.productName(),
         price: faker.commerce.price(),
-        image: faer.random.image(),
+        image: faker.random.image(),
         inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
         fastDelivery: faker.datatype.boolean(),
         ratings: faker.random.arrayElement([1, 2, 3, 4, 5])
