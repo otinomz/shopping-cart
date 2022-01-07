605e5794be3f92455f0f40c5fa1891cf0f5b459a
otinomz <awesomeotinomz@gmail.com>
2022-01-07 14:45:28 +0100

    wow wow

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {AiFillStar, AiOutlineStar} from "react-icons/ai"
 
 const Rating = ({rating, onClick, style}) => {
     return (
         <>
-            
+            {[...Array(5)].map((_, i) => (
+                <span>
+                    {rating > i ? (
+                       <AiFillStar fontSize="15px"/> 
+                    ) : (
+                        <AiOutlineStar fontSize="15px"/>
+                    )}
+                </span>
+           ))} 
         </>
     )
 }
