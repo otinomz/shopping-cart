457e0a5c6e963fe2fa7cb28400779d36951c10d5
otinomz <awesomeotinomz@gmail.com>
2022-01-07 13:37:50 +0100

    milestone baby

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { CartState } from '../context/Context'
 import SingleProduct from './SingleProduct'
+import Filters from "./Filters"
 import './style.css'
 
 const Home = () => {
@@ -12,7 +13,10 @@ const Home = () => {
  
     return (
         <div className="home">
-            {/* filters */}
+            {/* filters component to be added for
+                filtering the item
+            */}
+            <Filters/>
             <div className="productContainer">
                 {
                     products.map((product) => {
