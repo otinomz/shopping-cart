a4ade9d1b3be6f798b5728f91d66de864a3ce668
otinomz <awesomeotinomz@gmail.com>
2022-01-07 15:29:04 +0100

    new fixes

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 const SingleProduct = ({product}) => {
     return (
         <div className="product">
             <Card>
                 <Card.Img variant="top" src={product.image} alt={product.name }/>
+                <Card.Body>
+                    <Card.Title>{product.name}</Card.Title>
+                    <Card.Subtitle style={{ paddingBottom: 10 }}>
+                        <span>₦{product.price.split("."[0])}</span>
+                        {
+                            product.fastDelivery ? (
+                                <div>Fast Delivery</div>
+                            ) : (
+                                <div>4 days delivery</div>
+                            )
+                        }
+                        <Rating rating={product.ratings}/>
+                    </Card.Subtitle>
+                </Card.Body>
             </Card>
         </div>
     )
