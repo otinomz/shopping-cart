d7ef06896314e294891c8cca7595f6158d0fb48a
otinomz <awesomeotinomz@gmail.com>
2022-01-07 12:55:26 +0100

    new fix

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,24 +1,25 @@
-import {createContext} from "react"
+import {createContext, useEffect} from "react"
 import faker from "faker"
 
 const Cart = createContext()
 
 const Context = ({ children }) => {
     
-    //using faker package installed in the dependencies
+    // using faker package installed in the dependencies
     // to generate data
-    const products = [...Array(20).map(() => ({ 
-        id: faker.datatype.uuid(), 
+    const products = [...Array(20)].map(() => ({
+        id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: faker.commerce.price(),
         image: faker.random.image(),
         inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
         fastDelivery: faker.datatype.boolean(),
-        ratings: faker.random.arrayElement([1, 2, 3, 4, 5])
-    }))] 
-
+        ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+    }));
+    console.log(products)
+    
     return (
-        <Cart.Provider>
+        <Cart.Provider value={products}>
             {children}
         </Cart.Provider>
     )
