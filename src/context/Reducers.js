07ddb867986ac258e2cf0492f182ba532fcd54bd
otinomz <awesomeotinomz@gmail.com>
2022-01-07 16:15:01 +0100

    new fixes

--- src/context/Reducers.js
+++ src/context/Reducers.js
@@ -13,12 +13,4 @@ export const cartReducer = (state, action) => {
         default:
             return state;
     }  
-    // switch (action.type) {
-    // case "ADD_TO_CART":
-    //   return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] };
-    // case "REMOVE_FROM_CART":
-    //   return {
-    //     ...state,
-    //     cart: state.cart.filter((c) => c.id !== action.payload.id),
-    //   };
 } 
