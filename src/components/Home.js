288114a25a7a10948cc72fef7c0e84f8553076c7
otinomz <awesomeotinomz@gmail.com>
2022-01-07 13:26:02 +0100

    new update

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,23 @@ import React from 'react'
 import { CartState } from '../context/Context'
 
 const Home = () => {
-
-    const { state } = CartState()
+    const {
+        state: { products },
+    } = CartState()
     
-    console.log(state)
-
+    console.log(products)
+ 
     return (
-        <div>
-            home page
+        <div className="home">
+            {/* filters */}
+            <div className="productContainer">
+                {
+                    products.map((product) => {
+                       return <span key={product.id}>{product.name}</span>
+                    })
+                }
+            </div>
+
         </div>
     )
 }
